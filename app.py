4de293e8d02d9c8f4c6627609a4b7bffa0152b46
Vladimir Makutenko <vvmakutenko@gmail.com>
2021-08-08 14:05:10 +0300

    add about tab

--- app.py
+++ app.py
@@ -79,6 +79,35 @@ tab1_content = [dbc.Row([
     ])]
 tab2_content = [dbc.Row(html.Div(id="data-table"), style={"margin-top": 20})]
 
+#tab3 content
+table_header = [
+    html.Thead(html.Tr([html.Th("Field Name"), html.Th("Details")]))
+]
+expl = {"KOI": "Object of Interest number",
+        "A": "Semi-major axis (AU)",
+        "RPLANET": "Planetary radius (Earth radii)",
+        "RSTAR": "Stellar radius (Sol radii)",
+        "TSTAR": "Effective temperature of host star as reported in KIC (k)",
+        "KMAG": "Kepler magnitude (kmag)",
+        "TPLANET": "Equilibrium temperature of planet, per Borucki et al. (k)",
+        "T0": "Time of transit center (BJD-2454900)",
+        "UT0": "Uncertainty in time of transit center (+-jd)",
+        "PER": "Period (days)",
+        "UPER": "Uncertainty in period (+-days)",
+        "DEC": "Declination (@J200)",
+        "RA": "Right ascension (@J200)",
+        "MSTAR": "Derived stellar mass (msol)"}
+
+tbl_rows =[]
+for i in expl:
+    tbl_rows.append(html.Tr([html.Td(i), html.Td(expl[i])]))
+table_body = [html.Tbody(tbl_rows)]
+table = dbc.Table(table_header + table_body, bordered=True)
+text = 'Data are source from Kepler API via asterank.com'
+tab3_content = [dbc.Row(html.A(text, href='http://www.asterank.com/kepler'),
+                        style={'margin-top': 20}),
+                dbc.Row(html.Div(children=table), style={'margin-top': 20})]
+
 app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.LUX])
 
@@ -108,7 +137,7 @@ app.layout = html.Div([
     dbc.Tabs([
         dbc.Tab(tab1_content, label="Charts"),
         dbc.Tab(tab2_content, label="Data"),
-        dbc.Tab(html.Div("About app"), label="About")
+        dbc.Tab(tab3_content, label="About")
     ])
     ],
     style={"margin-left": "80px",
